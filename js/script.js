e64d70256b443586cf2bcf09bcac43f080319f99
Nazgulkor <74013888+Nazgulkor@users.noreply.github.com>
2021-10-07 11:56:17 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 "use strict";
 
-const back = document.querySelector('.container');
+const back = document.querySelector('body');
 const button = document.querySelector('.btn');
 const span = document.querySelector('.color');
-const colors = [
+const list = document.querySelector('.list');
+const listColors = document.querySelector('.colors');
+const paper = document.querySelector('.wellpaper');
+const closeIt = document.querySelector('.close');
+const showColor = document.querySelector('.wellpaper span');
+
+
+const addColor = document.createElement('div');
+addColor.style.cssText = `
+    width : 20px;
+    height : 20px;
+`;
+
+const coubs = [];
+
+const generateColors = [
     '0',
     '1',
     '2',
@@ -23,26 +38,43 @@ const colors = [
 ];
 
 
+
 (function () {
     back.style.backgroundColor = getColor();
     span.textContent = getColor();
-    span.style.color = back.style.backgroundColor
+    span.style.color = back.style.backgroundColor;
 })()
 
 
 function getColor(){
     let color = "#";
     for(let i = 0; i < 6; i++){
-        color += colors[Math.floor(Math.random() * colors.length)];
+        color += generateColors[Math.floor(Math.random() * generateColors.length)];
     }
-    return color
+    return color;
 }
 
-button.addEventListener('click', () =>{
+button.addEventListener('click', () => {
     back.style.backgroundColor = getColor();
     span.textContent = getColor();
     span.style.color = back.style.backgroundColor;
     button.style.color = back.style.backgroundColor;
+    const coub = document.createElement('div');
+    coub.className = "coubs";
+    coub.style.cssText = `
+        width : 20px;
+        height : 20px;
+        background-color : ${back.style.backgroundColor};
+        cursor: pointer;
+    `;
+    listColors.append(coub);
+    coubs.push(coub);
+    coubs.forEach((item) => {
+        item.addEventListener('click', () => {
+            paper.style.backgroundColor = item.style.backgroundColor;
+            showColor.textContent = item.style.backgroundColor
+        })
+    })
 });
 
 button.addEventListener('mouseover', () => {
@@ -52,6 +84,27 @@ button.addEventListener('mouseover', () => {
 button.onmouseout = function() {
     button.style.color = 'black';
 }
+
+list.addEventListener('click', () => {
+    listColors.classList.toggle("colors-active");
+    paper.classList.toggle("wellpaper-active");
+    closeIt.classList.toggle('close-active');
+    showColor.classList.toggle('show-color-active')
+})
+
+
+list.addEventListener('click', () =>{
+    paper.style.backgroundColor = '';
+    showColor.textContent = '';
+})
+closeIt.addEventListener('click', () => {
+    listColors.classList.remove('colors-active');
+    paper.classList.remove('wellpaper-active');
+    closeIt.classList.remove('close-active');
+    paper.style.backgroundColor = '';
+    showColor.textContent = '';
+})
+
 
 
 
